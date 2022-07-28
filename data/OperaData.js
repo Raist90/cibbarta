d5cfab509ceaadf52b386f880ff2b3efe0a87e9a
Raist90 <manuel.detoma@gmail.com>
2022-07-28 19:47:25 +0200

    added 26 images

--- data/OperaData.js
+++ data/OperaData.js
@@ -48,5 +48,85 @@ export const OperaData = [
         name: "Monnalisa ed il torroncino",
         url: "https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/284647747_733907424723959_848137522673623069_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=111&_nc_ohc=H9fq5H55lGIAX81j8P-&tn=dZHhGsV_UzBaIJJ1&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjg0OTUzMzEyMDUxNDkyMzgwMQ%3D%3D.2-ccb7-5&oh=00_AT_KTialij-8fwVzb6D1v8dnWXsUmCjrfSSV8IQJoFDXkQ&oe=62EA532F&_nc_sid=30a2ef",
         key: 10
+    },
+    {
+        name: "Capra ai 4 formaggi",
+        url: "https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/284226562_1652035965175060_7160292703888576602_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=104&_nc_ohc=g2BbeGLRK-gAX90ZqBm&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjg0ODc5MjE4MTQyOTQ5MTAzNQ%3D%3D.2-ccb7-5&oh=00_AT81BvtZVSymIqP6MKdpiwF3-QlwfzlYMCM0JcdghWopNw&oe=62E97002&_nc_sid=30a2ef",
+        key: 11
+    },
+    {
+        name: "Sogno di un choco fresh",
+        url: "https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/282447063_1080945119437206_7896717090234467586_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=106&_nc_ohc=sDqe4Dgrlr8AX98pXqL&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjg0MzcxNDEyNzcyODI4NTM3Mw%3D%3D.2-ccb7-5&oh=00_AT9VOc7xBAO4O-ahBhOfeSQlHhqYYB6abOCmrXD-8e4CWg&oe=62E99646&_nc_sid=30a2ef",
+        key: 12
+    },
+    {
+        name: "Moon mania",
+        url: "https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/280750654_1274670623274250_3089373632308727264_n.jpg?stp=dst-jpg_e35_s750x750_sh0.08&_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=110&_nc_ohc=FdCY8OgUZLAAX_wjyxp&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=MjgzODg3MjA4MTI1ODQ1MzkwMw%3D%3D.2-ccb7-5&oh=00_AT_cyBeOHYoBEfscORkpcY_bCuUkcWOBffzdWYoIv6JVTw&oe=62E96C6C&_nc_sid=30a2ef",
+        key: 13
+    },
+    {
+        name: "La creazione del pisellino",
+        url: "https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/279101247_3263365783986981_361163704215426424_n.jpg?stp=dst-jpg_e35_s750x750_sh0.08&_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=109&_nc_ohc=a67dRzGwZOEAX_p5wyE&tn=dZHhGsV_UzBaIJJ1&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=MjgyMzQzMjQwOTM2Nzc2NTIzNA%3D%3D.2-ccb7-5&oh=00_AT8q_xCT8KShOQ2m508Smj0VuHX3R6pUykOz7vrwU6nezQ&oe=62E8DC21&_nc_sid=30a2ef",
+        key: 14
+    },
+    {
+        name: "Buona Pasqua",
+        url: "https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/278579516_123210673647074_5073053838503162489_n.jpg?stp=dst-jpg_e35_s750x750_sh0.08&_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=V_cRlOMBWN8AX9E5xuY&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=MjgxODI3MjYyMjI2Mjk2MjQ4MQ%3D%3D.2-ccb7-5&oh=00_AT-KnfUmAAspAwZNdienyqrgaF2jkeqVpb3xp79Y1MskuA&oe=62E8BB37&_nc_sid=30a2ef",
+        key: 15
+    },
+    {
+        name: "Il frutto dell’amore",
+        url: "https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/278158113_2218005118351908_1281580965218665613_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=100&_nc_ohc=W_wsZXip5GcAX9Rd6vd&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=MjgxMzQxNzU2NDk3NTI0MzQyOA%3D%3D.2-ccb7-5&oh=00_AT87jFKpA5MLWPzb9XTYUmagimPBvuuRpLnJomJlc_9bUA&oe=62E9B3C4&_nc_sid=30a2ef",
+        key: 16
+    },
+    {
+        name: "Questo famoso quadro di Leonardo Da Vinci...",
+        url: "https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/277696389_1232378150503326_9009176606356905998_n.jpg?stp=dst-jpg_e35_s750x750_sh0.08&_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=106&_nc_ohc=kc4mKfOdHqQAX9FsRhs&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=MjgwNjY0OTEyNjkwNzg0NjIwMA%3D%3D.2-ccb7-5&oh=00_AT8C3llzkvOSNTZRiCa_mFlP8bAl3a9qrwfgs2NtPt0gRw&oe=62EA5D63&_nc_sid=30a2ef",
+        key: 17
+    },
+    {
+        name: "La festa delle donne è tutto l’anno",
+        url: "https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/275857186_4955994457841199_4198901919592429427_n.jpg?stp=dst-jpg_e35_s750x750_sh0.08&_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=0hdnoHBequsAX_hAwKG&tn=dZHhGsV_UzBaIJJ1&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjc5MzA3MjYyOTA1MTk5MTEyOQ%3D%3D.2-ccb7-5&oh=00_AT8ynbSOOviJmwFdjvlkwA5KIbIsN8zhwe0_yPz3l4xmSw&oe=62E937EC&_nc_sid=30a2ef",
+        key: 18
+    },
+    {
+        name: "Pepe vitruviano",
+        url: "https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/275218506_126289459966045_9005687285493311066_n.jpg?stp=dst-jpg_e35_p750x750_sh0.08&_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=101&_nc_ohc=IfmN9if4fuUAX-Fv3NZ&tn=dZHhGsV_UzBaIJJ1&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjc4ODAxNjUxMDYzODExNDAwOA%3D%3D.2-ccb7-5&oh=00_AT_jTWvQczHMy3DjXiluooO63Y9WmTKZqFJswxhyw19E6w&oe=62E97B11&_nc_sid=30a2ef",
+        key: 19
+    },
+    {
+        name: "Las meninas e cocco di neve",
+        url: "https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/274975443_5010923828975700_4609145757434878798_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=100&_nc_ohc=ZcbEGqaiFzMAX_5OgUf&tn=dZHhGsV_UzBaIJJ1&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjc4MjkzMjkxNzAwODA3NTIzOA%3D%3D.2-ccb7-5&oh=00_AT_6Vg__9SUxU6MrFNUznfki_INBybC2E2HC05kYJ3C2WQ&oe=62EA8DCC&_nc_sid=30a2ef",
+        key: 20
+    },
+    {
+        name: "Kinder fetta al letto di Van Gogh",
+        url: "https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/274211243_456501862842844_4912117475186397764_n.jpg?stp=dst-jpg_e35_s750x750_sh0.08&_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=104&_nc_ohc=4hpaFkDXzfQAX-dgGPn&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjc3Nzg2NTI3MDUyNDQ5NDk1Nw%3D%3D.2-ccb7-5&oh=00_AT-kADhrdZjp5KpG9fJIr93RoVxVz_kQZ39S1JWpEUMC1A&oe=62EA9C3C&_nc_sid=30a2ef",
+        key: 21
+    },
+    {
+        name: "Gli amanti",
+        url: "https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/273886648_363280635618241_4849583887172868040_n.jpg?stp=dst-jpg_e35_s750x750_sh0.08&_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=101&_nc_ohc=1yRKSjktvEMAX8WkEuH&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjc3MzMzNzk0OTEzODM1MDAxMA%3D%3D.2-ccb7-5&oh=00_AT8ENZ56W9mEyksPUFqtje2LXc_JFbAbglqJGRzMpTX85Q&oe=62E97620&_nc_sid=30a2ef",
+        key: 22
+    },
+    {
+        name: "Don’t touch my cheesecake",
+        url: "https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/273410932_337475214775842_8752260134209665896_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=106&_nc_ohc=syZ-wT8x658AX8GnRUh&tn=dZHhGsV_UzBaIJJ1&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjc2NzcyMDg3MzMwMDUwOTg3OQ%3D%3D.2-ccb7-5&oh=00_AT84IqFsFJ9N0c_Yh5twB7DzMwiSmua252bFbBWQ8ZDS1Q&oe=62E9C54D&_nc_sid=30a2ef",
+        key: 23
+    },
+    {
+        name: "Angels superstar",
+        url: "https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/272920688_340139661323714_7330091521932051524_n.jpg?stp=dst-jpg_e35_s750x750_sh0.08&_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=hnSNINLISi8AX8W_vE_&tn=dZHhGsV_UzBaIJJ1&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjc2MjgwMTAwMjY1MjMxMjMwOQ%3D%3D.2-ccb7-5&oh=00_AT8qZBWuN5aQY5kYvYB04UfTI4XjIPPC5Qd6zfIXVXWteQ&oe=62E9E97A&_nc_sid=30a2ef",
+        key: 24
+    },
+    {
+        name: "Arcimboldo, il primo del #cibosutela",
+        url: "https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/272337319_1432204457198581_716921179623108571_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=101&_nc_ohc=Ue5cvq8mpUEAX9B0CQM&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjc1NzU3ODU0NTg4MTU4NTgxMg%3D%3D.2-ccb7-5&oh=00_AT8-SanTcM_smi-qHKBMlCnj15oeqg2xB9uLMEY2oIlBjA&oe=62E9A309&_nc_sid=30a2ef",
+        key: 25
+    },
+    {
+        name: "Quando vuoi uscire a mangiare un profiterole, ma tutti i tuoi amici sono positivi!",
+        url: "https://scontent-mad1-1.cdninstagram.com/v/t51.2885-15/271915529_606556920448470_8969381390389982497_n.jpg?stp=dst-jpg_e35_s750x750_sh0.08&_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_cat=106&_nc_ohc=N75u64tdJdAAX8yPZLn&edm=ALQROFkBAAAA&ccb=7-5&ig_cache_key=Mjc1MjQ1NTQ4NzUyNTAwMjE3OA%3D%3D.2-ccb7-5&oh=00_AT_cx0xkPNWEIFgy3zp-kct9KVGzrWH-YiPCDxlQ9fWrzw&oe=62E94C9C&_nc_sid=30a2ef",
+        key: 26
     }
 ]
