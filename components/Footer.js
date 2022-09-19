39af3bad84629146836b7692e5aa430903c0b0b7
Raist90 <manuel.detoma@gmail.com>
2022-09-19 09:53:29 +0200

    added a to top button in footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,11 @@
 import { StyledFooter, StyledP } from './styles/Footer.styled'
+import ScrollToTop from "react-scroll-to-top";
+
 
 const Footer = () => {
     return (
         <StyledFooter>
+            <ScrollToTop smooth />
             <StyledP>
                 © <span>{(new Date().getFullYear())}</span> copyright Cibbarta | All rights reserved
             </StyledP>
