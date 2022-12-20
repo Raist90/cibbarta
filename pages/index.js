9644306715177e0adfeeb6f210fd9dc19a0b0dbd
Raist90 <manuel.detoma@gmail.com>
2022-12-20 16:20:22 +0100

    added lightbox to home pictures

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,50 @@ import { OperaData } from '../data/OperaData'
 import Image from 'next/image'
 import Footer from '../components/Footer'
 import { useState } from 'react'
-import FsLightbox from 'fslightbox-react'
 
 export default function Home({ title = "Cibbarta" }) {
-  const [toggler, setToggler] = useState(false);
+  const [lightboxDisplay, setLightboxDisplay] = useState(false)
+  const [imageToShow, setImageToShow] = useState("")
+
+  const showImage = (image) => {
+    setImageToShow(image);
+    setLightboxDisplay(true);
+  }
+
+  const hideLightBox = () => {
+    setLightboxDisplay(false)
+  }
+
+  const showNext = (e) => {
+    e.stopPropagation()
+    let currentIndex = images.indexOf(imageToShow)
+    if (currentIndex >= images.length - 1) {
+      setLightboxDisplay(false)
+    }
+    else {
+      let nextImage = images[currentIndex + 1]
+      setImageToShow(nextImage)
+    }
+  }
+
+  const showPrev = (e) => {
+    e.stopPropagation()
+    let currentIndex = images.indexOf(imageToShow)
+    if (currentIndex <= 0) {
+      setLightboxDisplay(false)
+    }
+    else {
+      let nextImage = images[currentIndex - 1]
+      setImageToShow(nextImage)
+    }
+  }
+
+  const images = OperaData.map(item => item.url)
+  const imageCards = images.map((image, index) => (
+    <div key={OperaData[index].key}>
+      <Image onClick={() => showImage(image)} src={image} fill sizes="100%" alt="immagine" />
+    </div>
+  ));
 
   return (
     <>
@@ -20,34 +60,23 @@ export default function Home({ title = "Cibbarta" }) {
         <Header />
 
         <Opera>
-          {OperaData.map((opera) => {
-            return (
-              <div key={opera.key}>
-                <Image
-                  onClick={() => setToggler(!toggler)}
-                  src={opera.url}
-                  alt={opera.name}
-                  fill sizes="100%"
-                />
-              </div>
-            )
-          })}
+          {imageCards}
         </Opera>
-        <FsLightbox
-          toggler={toggler}
-          sources={
-            OperaData.map((props) => {
-              return (
-                <span className='contienimi' key={props.key}>
-                  <img
-                    src={props.url}
-                    alt={props.name}
-                  />
-                </span>
-              )
-            })
-          }
-        />
+
+        {
+          lightboxDisplay ? (
+            <div id="lightbox" onClick={hideLightBox}>
+              <button onClick={showPrev}>&#8592;</button>
+              <Image
+                src={imageToShow}
+                alt="immagine"
+                width={400}
+                height={300}
+              />
+              <button onClick={showNext}>&#8594;</button>
+            </div>
+          ) : ""
+        }
       </main>
 
       <Footer />
